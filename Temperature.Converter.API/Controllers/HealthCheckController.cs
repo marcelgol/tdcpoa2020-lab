696db09345cda4c1f8054d5ad5856d21d4fdf9f3
Marcel Goldhardt <marcel_goldhardt@dell.com>
2020-12-02 15:15:45 -0300

    First commit

---
+++ Temperature.Converter.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Calculator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public HttpStatusCode Post()
+        {
+            return HttpStatusCode.OK;
+        }
+
+    }
+}
